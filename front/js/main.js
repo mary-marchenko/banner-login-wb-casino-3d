14e76e0b07afc574645bd4cadcc6085a9a9417b8
m.marchenko <m.marchenko@favbet.ua>
2025-11-13 14:58:18 +0200

    first

---
+++ front/js/main.js
@@ -0,0 +1,11 @@
+(function init100vh(){
+    function setHeight() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    setHeight();
+    window.addEventListener('resize', setHeight);
+})();
+window.addEventListener('orientationchange', () => {
+    window.location.reload();
+});
